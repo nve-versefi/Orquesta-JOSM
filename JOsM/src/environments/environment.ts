d97ac2c2903495775b5140cfcfeae6facdb074ad
nve-versefi <n.vaneerde@versefi.co.uk>
2024-01-21 12:52:45 +0000

    Updated production configurations

--- JOsM/src/environments/environment.ts
+++ JOsM/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000/api', // URL del servidor de desarrollo
+    production: true,
+    apiUrl: 'http://www.orquestajosm.com', // URL del servidor de desarrollo
   };
