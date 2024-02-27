a06e5a486f5d89623943c23dac10b35f67f8d9de
nve-versefi <n.vaneerde@versefi.co.uk>
2024-02-27 14:36:21 +0000

    Data services amend

--- JOsM/JOsM/src/app/services/data.service.ts
+++ JOsM/JOsM/src/app/services/data.service.ts
@@ -3,7 +3,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, catchError } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root',
