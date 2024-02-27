3a7a9aba4ea2fdd12bbe207a135a9bd965407120
nve-versefi <n.vaneerde@versefi.co.uk>
2024-02-27 14:45:06 +0000

    envirotnment change

--- JOsM/src/environments/environment.ts
+++ JOsM/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000' , // URL del servidor de desarrollo
+    apiUrl: 'https://josm-backend-6599f0686bc6.herokuapp.com' , // URL del servidor de desarrollo
   };
